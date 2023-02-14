d3d15a4a89a0977aa2ebe1809eb7b7953ed1905e
aline-01 <aaryn3942@gmail.com>
2023-02-14 06:03:36 +0330

    fix the whois modules

--- modules/whois.py
+++ modules/whois.py
@@ -1,17 +1,25 @@
 import requests
 import time
-from bs4 import BeautifulSoup
 
 def whois():
-   try:
-      target = input("target:")
-   except:
-      return
-   url = "https://whois.com/whois/"+str(target)
-   req = requests.get(url)
-   
-
-   try:
-      input("[+] back to menu ...")
-   except:
+    try:
+       target = input("target: ")
+    except:
+       return
+    post_data = {"remoteAddress":target}
+    try:
+       req = requests.post("https://www.yougetsignal.com/tools/whois-lookup/php/get-whois-lookup-json-data.php",data=post_data)
+       result = req.text.split("<br \/>")
+       result = result[1:-1]
+    except:
+       print("[!] can't connect to api")
+    print("")
+    for i in result:
+       i = i[2:1000]
+       print(i)
+       time.sleep(0.1)
+    try:
+       input("back to menu ...")
+    except:
        pass
+    
